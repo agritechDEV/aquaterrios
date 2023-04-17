ddc7db10c620a0625ccacd850e6decc46e5dded4
agritechDEV <helpdesk.agritech@hotmail.com>
2023-04-17 21:51:50 +0200

    remove general.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi.staticfiles import StaticFiles
 from route.users import user_router
 from webroutes.admin import web_router
 from route.protected import base_router
-from route.general import general_router
 from route.api import api_router
 
 origins = ["*"]
@@ -24,7 +23,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(user_router, tags=["users"])
 app.include_router(web_router)
 app.include_router(base_router, prefix="/base", tags=["base"])
-app.include_router(general_router, prefix="/general", tags=["general"])
 app.include_router(api_router, prefix="/api", tags=["API"])
 
 
