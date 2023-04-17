cf868d4cf9d136a72ea57f92dd2b02012febab5b
agritechDEV <helpdesk.agritech@hotmail.com>
2023-04-17 21:36:08 +0200

    changed auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 from datetime import datetime, timedelta
 import random
 import smtplib
@@ -60,7 +60,7 @@ def verify_password(plain_password, hashed_password):
 def get_hashed_password(password):
     return pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
